ace6469f88d3861639fb34898c5574b20cd1df3a
a21josperjim <114883143+a21josperjim@users.noreply.github.com>
2023-05-18 09:26:04 +0200

    Update FruitManager.cs

--- FruitManager.cs
+++ FruitManager.cs
@@ -1,1 +1,29 @@
-//Prueba
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FruitManager : MonoBehaviour
+{
+    public GameObject transition;
+    
+    private void Update()
+    {
+        AllFruitsCollected();
+    }
+
+
+    public void AllFruitsCollected()
+    {
+        if (transform.childCount==0)
+        {
+            transition.SetActive(true);
+            Invoke("ChangeScene", 0.5);
+        }
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
